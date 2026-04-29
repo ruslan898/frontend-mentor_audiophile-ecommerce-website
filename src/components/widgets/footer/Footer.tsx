fd301a50e630c4e5eb3c82a37349c33db1b18725
Ruslan Abibullaev <ruslan_abibullaev@mail.ru>
2026-04-29 17:07:12 +0500

    Add Footer and Header components with corresponding styles

---
+++ src/components/widgets/footer/Footer.tsx
@@ -0,0 +1,9 @@
+import styles from './Footer.module.scss'
+
+export default function Footer() {
+  return (
+    <header>
+      <h1>Footer</h1>
+    </header>
+  );
+}
---
+++ src/components/widgets/header/Header.tsx
@@ -0,0 +1,7 @@
+import styles from './Header.module.scss'
+
+export default function Header() {
+  return <header>
+    <h1>Header</h1>
+  </header>
+}
