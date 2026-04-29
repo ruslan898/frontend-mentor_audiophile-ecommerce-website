69400a825c57b2fc89d5f1f085d710d1b921cca2
Ruslan Abibullaev <ruslan_abibullaev@mail.ru>
2026-04-29 17:04:20 +0500

    Implement main App component with routing and remove old App component

--- src/components/App.tsx
+++
@@ -1,5 +0,0 @@
-import styles from './App.module.scss'
-
-export default function App() {
-  return <h1>Hello World</h1>
-}
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './main.scss'
-import App from './components/App';
+import App from './app/App';
 
 const root = document.querySelector('#root');
 
