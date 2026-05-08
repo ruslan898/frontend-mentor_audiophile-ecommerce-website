81429cd031e85eeb91776c59520e2e023fee2927
Ruslan Abibullaev <ruslan_abibullaev@mail.ru>
2026-05-09 00:44:20 +0500

    Add ProductPreview component with styles and responsive design

---
+++ src/components/widgets/productPreview/ProductPreview.tsx
@@ -0,0 +1,30 @@
+import Title from '../../ui/title/Title';
+import Button from '../../ui/button/Button';
+import styles from './ProductPreview.module.scss';
+import sectionImage from '../../../assets/product-xx99-mark-two-headphones/mobile/image-category-page-preview.jpg';
+
+export default function ProductPreview() {
+  return (
+    <section className={styles.productPreview}>
+      <div className="container">
+        <div className={styles.productPreviewWrapper}>
+          <div className={styles.imageBox}>
+            <img src={sectionImage} alt="Headphones" />
+          </div>
+          <div className={styles.description}>
+            <p className={styles.overline}>New product</p>
+            <Title level={2} variant="lg">
+              XX99 Mark II Headphones
+            </Title>
+            <p className={styles.text}>
+              The new XX99 Mark II headphones is the pinnacle of pristine audio.
+              It redefines your premium headphone experience by reproducing the
+              balanced depth and precision of studio-quality sound.
+            </p>
+            <Button variant="filled">See product</Button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
