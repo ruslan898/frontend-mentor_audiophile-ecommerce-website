890c47cbf4ca064ea731fb5ea3369ff26b7a4efd
Ruslan Abibullaev <ruslan_abibullaev@mail.ru>
2026-05-09 00:43:12 +0500

    Refactor Button component variants and styles for consistency

--- src/components/ui/button/Button.tsx
+++ src/components/ui/button/Button.tsx
@@ -4,11 +4,12 @@ import styles from './Button.module.scss'
 
 type ButtonProps = {
   children: React.ReactNode;
-  variant: 'primary' | 'secondary' | 'alternative';
+  variant: 'filled' | 'outline' | 'text';
+  className?: string
 };
 
-export default function Button({ children, variant, ...props }: ButtonProps) {
-  const classes = clsx(styles.btn, styles[`btn-${variant}`])
+export default function Button({ children, variant, className, ...props }: ButtonProps) {
+  const classes = clsx(styles.btn, styles[`btn-${variant}`], className)
 
   return <button className={classes} {...props}>{ children }</button>
 }
