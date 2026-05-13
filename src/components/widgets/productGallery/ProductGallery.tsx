3dee693582a8eb1bbe02df800fb46e8431abe5d5
Ruslan Abibullaev <ruslan_abibullaev@mail.ru>
2026-05-14 02:31:45 +0500

    Refactor ProductGallery component to use props for image paths and update styling class names for consistency

--- src/components/widgets/productGallery/ProductGallery.tsx
+++ src/components/widgets/productGallery/ProductGallery.tsx
@@ -1,19 +1,36 @@
+import { useImageByScreenSize } from '../../../hooks/useImageByScreenSize';
+import type { ImagePaths } from '../../../types/types';
 import styles from './ProductGallery.module.scss';
-import img1 from '/assets/product-xx59-headphones/mobile/image-gallery-1.jpg';
-import img2 from '/assets/product-xx59-headphones/mobile/image-gallery-2.jpg';
-import img3 from '/assets/product-xx59-headphones/mobile/image-gallery-3.jpg';
 
-export default function ProductGallery() {
+type ProductGalleryProps = {
+  imagesObj: {
+    first: ImagePaths;
+    second: ImagePaths;
+    third: ImagePaths;
+  };
+};
+
+export default function ProductGallery({ imagesObj }: ProductGalleryProps) {
+  const { first, second, third } = imagesObj;
+
+  const img1 = useImageByScreenSize(first);
+  const img2 = useImageByScreenSize(second);
+  const img3 = useImageByScreenSize(third);
+
   return (
     <div className={styles.productGallery}>
-      <div className={styles.galleryTile}>
-        <img src={img1} alt="Img1" />
-      </div>
-      <div className={styles.galleryTile}>
-        <img src={img2} alt="Img2" />
-      </div>
-      <div className={`${styles.galleryTile} ${styles.big}`}>
-        <img src={img3} alt="Img3" />
+      <div className="container">
+        <div className={styles.productGalleryWrapper}>
+          <div className={styles.galleryTile}>
+            <img src={img1} alt="Img1" />
+          </div>
+          <div className={styles.galleryTile}>
+            <img src={img2} alt="Img2" />
+          </div>
+          <div className={`${styles.galleryTile} ${styles.big}`}>
+            <img src={img3} alt="Img3" />
+          </div>
+        </div>
       </div>
     </div>
   );
