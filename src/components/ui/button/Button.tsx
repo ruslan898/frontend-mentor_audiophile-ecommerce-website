8d8ca2f8b7eca363ef5dd9dd34cec45c471e2afe
Ruslan Abibullaev <ruslan_abibullaev@mail.ru>
2026-05-13 21:54:10 +0500

    Refactor Button component to include href prop and enhance type handling for better flexibility

--- src/components/ui/button/Button.tsx
+++ src/components/ui/button/Button.tsx
@@ -1,15 +1,41 @@
-import React from "react"
-import clsx from "clsx";
-import styles from './Button.module.scss'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import clsx from 'clsx';
+import styles from './Button.module.scss';
 
 type ButtonProps = {
+  href?: string;
   children: React.ReactNode;
   variant: 'filled' | 'outline' | 'text';
-  className?: string
+  className?: string;
+  type?: 'link' | 'button';
 };
 
-export default function Button({ children, variant, className, ...props }: ButtonProps) {
-  const classes = clsx(styles.btn, styles[`btn-${variant}`], className)
+export default function Button({
+  href,
+  children,
+  variant,
+  className,
+  type,
+  ...props
+}: ButtonProps) {
+  const classes = clsx(styles.btn, styles[`btn-${variant}`], className);
 
-  return <button className={classes} {...props}>{ children }</button>
+  if (type === 'button') {
+    return (
+      <button type="button" className={classes} {...props}>
+        {children}
+      </button>
+    );
+  }
+
+  if (!href) {
+    throw new Error('Please add a "href" property to the Link component')
+  }
+
+  return (
+    <Link to={href} className={classes} {...props}>
+      {children}
+    </Link>
+  );
 }
