b64e2acd1ff1ff809b54af0b2fc028aa0f88cf56
Ruslan Abibullaev <ruslan_abibullaev@mail.ru>
2026-05-16 12:24:55 +0500

    Add CartModal component with styling and structure; implement product list and checkout button

---
+++ src/components/modals/cartModal/CartModal.tsx
@@ -0,0 +1,39 @@
+import Button from '../../ui/button/Button';
+import CartItem from '../../ui/cartItem/CartItem';
+import Title from '../../ui/title/Title';
+import styles from './CartModal.module.scss';
+
+export default function CartModal() {
+  return (
+    <div className={styles.cartModal}>
+      <div className={styles.header}>
+        <Title level={4} variant="sm" className={styles.title}>
+          Cart (3)
+        </Title>
+        <button type="button" className={styles.clearCartBtn}>
+          Remove all
+        </button>
+      </div>
+      <ul className={styles.productsList}>
+        <li>
+          <CartItem />
+        </li>
+        <li>
+          <CartItem />
+        </li>
+        <li>
+          <CartItem />
+        </li>
+      </ul>
+      <div className={styles.summary}>
+        <div className={styles.total}>
+          <span className={styles.totalText}>Total</span>
+          <span className={styles.totalPrice}>$ 5,396</span>
+        </div>
+      </div>
+      <Button href="/checkout" variant="filled" className={styles.cartBtn}>
+        Checkout
+      </Button>
+    </div>
+  );
+}
