d010fedf6444a8d78288de00f071fcf2df956cb7
Ruslan Abibullaev <ruslan_abibullaev@mail.ru>
2026-05-18 01:35:27 +0500

    Refactor CartItem component to accept variant prop and conditionally render CartAmount or quantity

--- src/components/ui/cartItem/CartItem.tsx
+++ src/components/ui/cartItem/CartItem.tsx
@@ -1,18 +1,30 @@
 import CartAmount from '../cartAmount/CartAmount';
-import styles from './CartItem.module.scss'
-import cartItemImg from '/assets/cart/image-xx99-mark-two-headphones.jpg'
+import styles from './CartItem.module.scss';
+import cartItemImg from '/assets/cart/image-xx99-mark-two-headphones.jpg';
 
-export default function CartItem() {
+type CartItemProps = {
+  variant: 'cart' | 'checkout';
+};
+
+export default function CartItem({ variant }: CartItemProps) {
   return (
     <div className={styles.cartItem}>
       <div className={styles.info}>
-        <img src={cartItemImg} alt="An image of a product" className={styles.cartItemImg} />
+        <img
+          src={cartItemImg}
+          alt="An image of a product"
+          className={styles.cartItemImg}
+        />
         <div className={styles.textBlock}>
           <h5 className={styles.title}>XX99 MK II</h5>
           <p className={styles.price}>$ 2,999</p>
         </div>
       </div>
-      <CartAmount variant='cartModal' />
+      {variant === 'cart' ? (
+        <CartAmount variant="cartModal" />
+      ) : (
+        <span>x1</span>
+      )}
     </div>
   );
 }
