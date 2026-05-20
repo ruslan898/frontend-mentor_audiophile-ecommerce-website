4dba4c06196db055ca05624a1493d352bf00577a
Ruslan Abibullaev <ruslan_abibullaev@mail.ru>
2026-05-20 18:07:25 +0500

    Update CategoryLayout to dynamically display category name in subheader

--- src/pages/CategoryLayout.tsx
+++ src/pages/CategoryLayout.tsx
@@ -1,10 +1,13 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import CategoriesSubheader from '../components/ui/categoriesSubheader/CategoriesSubheader';
 
 export default function CategoryLayout() {
+  const path = useLocation().pathname;
+  const categoryName = path.split('/').pop();
+
   return (
     <>
-      <CategoriesSubheader>Headphones</CategoriesSubheader>
+      <CategoriesSubheader>{categoryName}</CategoriesSubheader>
       <Outlet />
     </>
   );
